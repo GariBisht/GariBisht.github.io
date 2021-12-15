d654f7d0092429bd06460854d84453fcddbcb2e3
Garima Bisht <74089873+GariBisht@users.noreply.github.com>
2021-12-15 18:51:36 +0530

    Added Initial Portfolio
    First version of My Portfolio Website

---
+++ assets/js/education.js
@@ -0,0 +1,151 @@
+AOS.init();
+
+// MOOCs Cards
+
+const moocs = document.querySelector(".moocs");
+const moocscards = [
+  {
+    title: "Machine Learning",
+    cardImage: "assets/images/education-page/coursera2.svg",
+    moocLink: "https://www.coursera.org/learn/machine-learning",
+  },
+  {
+    title: "Introduction to HTML 5",
+    cardImage: "assets/images/education-page/udemy.svg",
+    moocLink: "https://www.udacity.com/course/intro-to-html-and-css--ud001",
+  },
+  {
+    title: "Machine Learning",
+    cardImage: "assets/images/education-page/coursera2.svg",
+    moocLink: "https://www.coursera.org/learn/machine-learning",
+  },
+  {
+    title: "Introduction to HTML 5",
+    cardImage: "assets/images/education-page/udemy.svg",
+    moocLink: "https://www.udacity.com/course/intro-to-html-and-css--ud001",
+  },
+];
+
+const experience = [
+  {
+    img: "assets/images/education-page/c1.png"
+  },
+  {
+    img: "assets/images/education-page/c2.jpg"
+  },
+  {
+    img: "assets/images/education-page/c3.png"
+  },
+  {
+    img: "assets/images/education-page/c4.png"
+  },
+  {
+    img: "assets/images/education-page/c5.jpg"
+  },
+];
+
+let currentItem = 0;
+
+const img = document.getElementById('image');
+
+const prevBtn = document.querySelector('#prevBtn');
+const nextBtn = document.querySelector('#nextBtn');
+
+window.addEventListener('DOMContentLoaded', function () {
+  showExperience();
+})
+
+function showExperience() {
+  setInterval(function () {
+    if (currentItem === experience.length) {
+      currentItem = 0;
+    }
+      const item = experience[currentItem];
+      img.src = item.img;
+      currentItem++;
+
+    },
+    3000);
+}
+
+const showCards = () => {
+  let output = "";
+  moocscards.forEach(
+    ({ title, cardImage, moocLink }) =>
+    (output += `        
+        <div class="col-6 col-md-3 col-sm-4 column" data-aos="fade-up" data-aos-easing="linear" data-aos-delay="600" >  
+            <div class="card mb-3 mx-auto">
+               <div class="content">
+                  <div class="content-overlay"></div>
+                    <img src=${cardImage} class="card-img-top content-image">     
+                  <div class="content-details fadeIn-bottom">
+                    <a href="${moocLink}" target="_blank"><i class="fa fa-info-circle fa-2x" aria-hidden="true" style="color: white;"></i></a>                                   
+                  </div>
+                </div>
+                <div class="card-body">
+                    <h6 class="mt-0 py-2 text-center font-weight-bold mooc-title" style="font-size:12px;">${title}</h6>
+                </div>
+            </div>
+        </div>        
+      `)
+  );
+  moocs.innerHTML = output;
+};
+document.addEventListener("DOMContentLoaded", showCards);
+
+
+
+
+/* Timeline Section*/
+
+$(function(){
+
+  window.sr = ScrollReveal();
+
+  if ($(window).width() < 768) {
+
+    if ($('.timeline-content').hasClass('js--fadeInLeft')) {
+      $('.timeline-content').removeClass('js--fadeInLeft').addClass('js--fadeInRight');
+    }
+
+    sr.reveal('.js--fadeInRight', {
+      origin: 'right',
+      distance: '300px',
+      easing: 'ease-in-out',
+      duration: 800,
+    });
+
+  } else {
+    
+    sr.reveal('.js--fadeInLeft', {
+      origin: 'left',
+      distance: '300px',
+      easing: 'ease-in-out',
+      duration: 800,
+    });
+
+    sr.reveal('.js--fadeInRight', {
+      origin: 'right',
+      distance: '300px',
+      easing: 'ease-in-out',
+      duration: 800,
+    });
+
+  }
+  
+  sr.reveal('.js--fadeInLeft', {
+      origin: 'left',
+      distance: '300px',
+      easing: 'ease-in-out',
+      duration: 800,
+    });
+
+    sr.reveal('.js--fadeInRight', {
+      origin: 'right',
+      distance: '300px',
+      easing: 'ease-in-out',
+      duration: 800,
+    });
+
+
+});
---
+++ assets/js/experience.js
@@ -0,0 +1,70 @@
+AOS.init();
+
+//  Work experience cards
+
+
+const experiencecards = document.querySelector(".experience-cards");
+const exp = [
+  {
+    title: "LGM LetsGrowMore",
+    cardImage: "assets/images/experience-page/lgm.jpeg",
+    place: "Web Developer Intern",
+    time: "(Oct, 2021 - Nov, 2021)",
+    desp: "Did Virtual Internship with LetsGrowMore in Web Development. We had to do tasks of creating single page website and react based web app during this 1 month internship period",
+  },
+];
+
+const showCards2 = () => {
+  let output = "";
+  exp.forEach(
+    ({ title, cardImage, place, time, desp }) =>
+      (output += `        
+    <div class="col gaap" data-aos="fade-up" data-aos-easing="linear" data-aos-delay="100" data-aos-duration="400"> 
+      <div class="card card1">
+        <img src="${cardImage}" class="featured-image"/>
+        <article class="card-body">
+          <header>
+            <div class="title">
+              <h3>${title}</h3>
+            </div>
+            <p class="meta">
+              <span class="pre-heading">${place}</span><br>
+              <span class="author">${time}</span>
+            </p>
+            <ol>
+              ${desp}
+            </ol>
+          </header>
+        </article>
+      </div>
+    </div>
+      `)
+  );
+  experiencecards.innerHTML = output;
+};
+document.addEventListener("DOMContentLoaded", showCards2);
+
+
+
+
+const showCards3 = () => {
+  let output = "";
+  mentor.forEach(
+    ({ title, image, subtitle, desp}) =>
+      (output += `  
+      <div class="blog-slider__item swiper-slide">
+        <div class="blog-slider__img">
+            <img src="${image}" alt="">
+        </div>
+        <div class="blog-slider__content">
+          <div class="blog-slider__title">${title}</div>
+          <span class="blog-slider__code">${subtitle}</span>
+          <div class="blog-slider__text">${desp}</div>
+          <a href="#" class="blog-slider__button">Read More</a>   
+        </div>
+      </div>
+      `)
+  );
+  hackathonsection.innerHTML = output;
+};
+document.addEventListener("DOMContentLoaded", showCards3);
